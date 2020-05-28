b30dca16878418cb344483e8b50d35a490731087
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-05-28 14:11:30 -0500

    eliminacion de clases no necesarias

--- template/menu.php
+++ template/menu.php
@@ -2,10 +2,9 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a  href="<?php echo RUTA_BASE; ?>" class="brand-link">
-      <img src="<?php echo RUTA_PROYECTO ?>template/adminlte3/dist/img/AdminLTELogo.png"
-           class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light" <?php echo COLORBASE; ?>><b><?= NOMBRE_PROYECTO ?></b></span>
+      <div class="text-center">
+        <span  class="brand-text text-center font-weight-light" <?php echo COLORBASE; ?>><b><?= NOMBRE_PROYECTO ?></b></span>
+      </div>
     </a>
 
     <!-- Sidebar -->
