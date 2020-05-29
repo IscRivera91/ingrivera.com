a021aef1ecad222db8b013fd27f9fa91b7dff592
Ricardo Rivera Sanchez <45610577+IscRivera91@users.noreply.github.com>
2020-05-29 13:25:06 -0500

    Delete config.php

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// config RUTA
-	define('RUTA_PROYECTO','http://localhost/ingrivera.com/');
-	define('NOMBRE_PROYECTO','Ing Rivera');
-	define('RUTA_BASE',RUTA_PROYECTO.'index.php');
-
-    // COLORES
-    define('COLORBASE','style="color:#17a2b8"');
-	define('COLORBASE_BOOTSTRAP','info');
