6ab3b47289d3c82e6508a823dbab352b653bcec1
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-06-23 23:10:46 -0500

    .

--- index.php
+++ index.php
@@ -43,9 +43,9 @@ $menu = $menus->crear_menu();
 
 <footer class="main-footer">
     <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.0.0
+        <b>Version</b> 1.0.0
     </div>
-    <strong>Copyright © 2020 Ing Rivera . </strong>todos los derechos reservados.
+    <strong>Copyright © 2020 Ing Rivera . </strong>
 </footer>
 
 </div>
