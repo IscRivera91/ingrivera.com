d1e1955c679d919220b9afe389b40db4126dd684
Ricardo Rivera <ricardorivera.personal.91@gmail.com>
2020-10-31 19:48:11 -0600

    creando componenstes primarios de la nueva plantilla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/resume/header/header.component';
+import { IntroComponent } from './components/resume/intro/intro.component';
+import { BodyComponent } from './components/resume/body/body.component';
+import { FooterComponent } from './components/resume/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    IntroComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
