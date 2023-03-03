245501db236a38e17ee6976ac76861879bd3ec09
Ricardo Rivera <ricardo.rivera@indava.com>
2023-03-03 17:19:18 -0600

    translations

--- components/body/experiencia.php
+++ components/body/experiencia.php
@@ -1,11 +1,22 @@
 <section class="work-section py-3">
     <h3 class="text-uppercase resume-section-heading mb-4">WORK EXPERIENCE</h3>
-    
+
+    <div class="item mb-3">
+        <div class="item-heading row align-items-center mb-2">
+            <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Backend Software Engineer</h4>
+            <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Indava | 2022 - 2023</div>
+        </div>
+        <div class="item-content">
+            <p class="text-justify">
+                I worked in many projects with Drupal and Laravel.
+            </p>
+        </div>
+    </div><!--//item-->
+
     <div class="item mb-3">
       <div class="item-heading row align-items-center mb-2">
         <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Freelance Software Engineer</h4>
         <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Ing Rivera | 2020 - 2022</div>
-        
       </div>
       <div class="item-content">
           <p class="text-justify">
@@ -19,7 +30,6 @@
       <div class="item-heading row align-items-center mb-2">
         <h4 class="item-title col-12 col-md-6 col-lg-8 mb-2 mb-md-0">Back-End Developer PHP</h4>
         <div class="item-meta col-12 col-md-6 col-lg-4 text-muted text-left text-md-right">Alfabai | 2019 - 2020</div>
-        
       </div>
       <div class="item-content">
         <p class="text-justify">
