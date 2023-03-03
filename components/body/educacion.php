3cf4f1cd0de5be0b746c892731a5dff874fc5638
Ricardo Rivera <ricardo.rivera@indava.com>
2023-03-03 17:12:48 -0600

    translations

--- components/body/educacion.php
+++ components/body/educacion.php
@@ -1,15 +1,10 @@
 <section class="education-section py-3">
-    <h3 class="text-uppercase resume-section-heading mb-4">Educacion</h3>
+    <h3 class="text-uppercase resume-section-heading mb-4">Education</h3>
     <ul class="list-unstyled resume-education-list">
       <li class="mb-3">
         <div class="resume-degree font-weight-bold">Ingenieria en Sistemas Computacionales</div>
         <div class="resume-degree-org text-muted">Instituto Tecnologico Superior de Jesus Carranza</div>
         <div class="resume-degree-time text-muted">2012 - 2017</div>
-      </li>
-      <li>
-        <div class="resume-degree font-weight-bold">Ingenieria en Instrumentacion Electronica</div>
-        <div class="resume-degree-org text-muted">Universidad Veracruzana</div>
-        <div class="resume-degree-time text-muted">2010 - 2012</div>
       </li>
     </ul>
 </section><!--//education-section-->
--- components/body/habilidades.php
+++ components/body/habilidades.php
@@ -1,24 +1,20 @@
 <section class="skills-section py-3">
-    <h3 class="text-uppercase resume-section-heading mb-4">Habilidades</h3>
+    <h3 class="text-uppercase resume-section-heading mb-4">Skills</h3>
     <div class="item">
-        <h4 class="item-title">Tecnicas</h4>
+        <h4 class="item-title">Techniques</h4>
         <ul class="list-unstyled resume-skills-list">
-          <li class="mb-2">Administracion LAMP en Ubuntu Server</li>
-          <li class="mb-2">PHP/PHPUnit/MVC/OOP/Laravel<li>
-          <li class="mb-2">HTML</li>
+          <li class="mb-2">Digitalocean/</li>
+          <li class="mb-2">PHP/Pest/Laravel<li>
           <li class="mb-2">CSS/Bootstrap/AdminLTE3</li>
-          <li class="mb-2">JavaScript/TypeScript/Angular</li>
           <li class="mb-2">MySQL</li>
-          <li class="mb-2">GIT/GITHUB</li>
+          <li class="mb-2">GIT/GITHUB/GITHUB ACTION</li>
         </ul>
     </div>
     <div class="item">
-        <h4 class="item-title">Profesionales</h4>
+        <h4 class="item-title">Professionals</h4>
         <ul class="list-unstyled resume-skills-list">
-          <li class="mb-2">Comunicacion efectiva</li>
-          <li class="mb-2">Trabajo en equipo<li>
-          <li class="mb-2">Resolucion de problemas</li>
-          <li class="mb-2">Aprendizaje continuo</li>
+          <li class="mb-2">Understanding of cloud infrastructure</li>
+          <li class="mb-2">Team work<li>
         </ul>
     </div><!--//item-->
 </section><!--//skills-section-->
